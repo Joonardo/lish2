a541aaab136d1c1c3684cb9f43088edda040df73
Joonardo <joonardob@gmail.com>
2018-01-04 17:39:05 +0200

    Added CORS.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 var server = express()
+
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 server.use(bodyParser.json())
 
 server.get('/new_url', Pages.new_url)
