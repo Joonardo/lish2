6e64879b7dc0a47e2ac1ac84d1aa6dfca2ad31f3
Joonardo <joonardob@gmail.com>
2018-01-04 17:58:07 +0200

    Added some visualizations.

--- client/src/Fields.jsx
+++ client/src/Fields.jsx
@@ -1,41 +1,90 @@
 import React, {Component} from 'react';
-import {FormControl, FormGroup, HelpBlock, Collapse, Well} from 'react-bootstrap';
+import {FormControl, FormGroup, Collapse, Well} from 'react-bootstrap';
+
+import Api from './Api.jsx';
 
 class Short extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            value: ""
+            value: "",
+            validation: "success",
+            placeholder: ""
         };
+
+        this.error = ''
+
         this.changed = this.changed.bind(this);
-        this.getValidationState = this.getValidationState.bind(this)
+        this.isValid = this.isValid.bind(this);
     }
 
-    changed(ev) {
+    async componentDidMount() {
+        const ph = await Api.placeholder()
+        this.setState( Object.assign(
+            this.state,
+            {placeholder: ph}
+        ))
+    }
+
+    isValid() {
+        return this.state.validation === 'success'
+    }
+
+    async changed(ev) {
+        console.log(this.placeholder);
         this.setState(Object.assign(
             this.state,
             {value: ev.target.value}
         ))
-    }
 
-    async getValidationState() {
-        if(this.state.value.length > 2) return 'success';
-        if(this.state.value.length > 0) return 'error';
-        return null;
+        if(this.state.value.length === 0) {
+            this.setState( Object.assign(
+                this.state,
+                {validation: 'success'}
+            ))
+            return;
+        }
+
+        if(!/^[a-zA-Z0-9]+$/.test(this.state.value)) {
+            this.error = 'Short url should only contain a-z, A-Z or 0-9.'
+            this.setState( Object.assign(
+                this.state,
+                {validation: 'error'}
+            ))
+            return;
+        }
+
+        const exists = await Api.exists(this.state.value)
+        if(exists) {
+            this.error = 'Short url already exists.'
+            this.setState( Object.assign(
+                this.state,
+                {validation: 'error'}
+            ))
+            return;
+        }
+
+        this.setState( Object.assign(
+            this.state,
+            {validation: 'success'}
+        ))
+
     }
 
     render() {
         return (
-            <FormGroup validationState={this.getValidationState()}>
+            <FormGroup validationState={this.state.validation}>
                 <FormControl
                     type="text"
                     onChange={this.changed}
                     value={this.state.value}
+                    placeholder={this.state.placeholder}
                 />
                 <FormControl.Feedback />
 
-                <Collapse in={this.getValidationState() === 'error'}>
-                    <div><Well bsSize="small">Short url already in use.</Well></div>
+                <Collapse in={this.state.validation === 'error'}>
+                    <div><Well bsSize="small">{this.error}</Well></div>
                 </Collapse>
             </FormGroup>
         )
